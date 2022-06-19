64f976beb1a5e91835fb03c5321bcfadc3efd076
Sam <samuelnonyelu2000@Gmail.com>
2022-06-19 18:35:25 +0100

    fixed navbar
    everything seems fine. remains to test

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
+//Open menu
+
+
 const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelectorAll(".nav-menu");
+const navMenu = document.querySelector(".nav-menu");
 
 hamburger.addEventListener("click", mobileMenu);
 
 function mobileMenu() {
     hamburger.classList.toggle("active");
-    navMenu.forEach(e => e.classList.toggle("active"))
+    navMenu.classList.toggle("active");
 }
 
 
-// separate
+// close menu
 
 
 const navLink = document.querySelectorAll(".nav__link");
@@ -20,3 +23,17 @@ function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }
+
+// jquery to make the navbar stick to the top
+
+$(window).scroll(function () {
+    if($(window).scrollTop() > 400) {
+      $('.header--sticky').css('position', 'sticky');
+      $('.header--sticky').css('top', 0);
+      $('.header__nav').css("opacity", 0.9);
+    } else {
+         $('.header__nav').css('position', '');
+    //   $('.header__nav').css('top', '-100%');
+      $('.header__nav').css("opacity", 1);
+    }
+  })
